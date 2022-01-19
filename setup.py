7264185a081ab9cd6120947d89895a16407efafd
Elias Hoerner <elias.hoerner@hoerner-it.net>
2022-01-19 12:43:04 +0100

    release 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='resensepy',
-    version='0.0.1',
+    version='0.0.2',
     packages=['resensepy'],
     package_dir={'resensepy': 'src/resense'},
     url='http://resense.io/',
